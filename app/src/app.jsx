501472a663bf9099caed6d83cc07b9de20b47797
Alexandre Antonio Juca <corextechnologies@gmail.com>
2023-06-04 23:41:31 +0100

    Prepare to support shad-cn

--- app/src/app.jsx
+++ app/src/app.jsx
@@ -1,12 +1,15 @@
 import React from "react";
-import Button from "../src/components/Button"
+import Hello from "../src/components/Hello"
 import "./index.css";
+import { Camera } from "lucide-react";
 
 export default function MyApp() {
     const world = ", World"
     return (
         <>
             <h1 className="p-8 text-cyan-700">Hello {world}</h1>
+            <Hello></Hello>
+            <Camera color="red" size={48} />;
         </>
     )
 }
